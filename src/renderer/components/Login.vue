2ab9a623792f687fbd8db86252e506b6e69a7f6b
Leonardo galindo <naturalmysticxd@gmail.com>
2019-10-28 13:35:23 -0430

    Proyecto listo y nuevo para comenzar.

---
+++ src/renderer/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="wrapper">
+   <h1>Toposat app Electron Inicial</h1>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'login',
+    components: { SystemInformation },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
