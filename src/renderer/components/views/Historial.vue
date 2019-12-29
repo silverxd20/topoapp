97c2647e5de5a658c6a5c96a930fa671c041c374
Leonardo galindo <naturalmysticxd@gmail.com>
2019-12-29 14:07:25 -0400

    agregados los 2 componentes e importados

--- src/renderer/components/views/Historial.vue
+++ src/renderer/components/views/Historial.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
-
+        <historialPagoPaypal></historialPagoPaypal>
     </div>
 </template>
 
 <script>
+import historialPagoPaypal from '../miniComponents/HistorialComponent/historialPagoPaypal';
+import historialTareas from '../miniComponents/HistorialComponent/historialTareas';
+
     export default {
-        
+        components: {
+            historialPagoPaypal,
+            historialTareas,
+        },
     }
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
 
 </style>
